52be30c1eeaf9a3a51942572f680d1e561de2918
Augie Bren <Augie>
2022-05-16 22:24:17 -0500

    the home page

--- views/default.jsx
+++ views/default.jsx
@@ -1,12 +1,14 @@
 const React = require('react')
 function Def (html) {
-    <html>
-        <head>
-            <title>The Title is here</title>
-        </head>
-        <body>
-            {html.children}
-        </body>
-    </html>
+    return (
+        <html>
+            <head>
+                <title>The Title is here</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
 }
 module.exports = Def
---
+++ views/home.jsx
@@ -0,0 +1,12 @@
+const React = require('require')
+const Def = require('./default')
+function home () {
+    return (
+        <DEF>
+            <main>
+                <h1>H1 on Home page</h1>
+            </main>
+        </DEF>
+    )
+}
+module.exports = home
