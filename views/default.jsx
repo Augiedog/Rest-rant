950598318101fa0e7e8c6c6a4a69c0303be6bfbc
Augie Bren <Augie>
2022-05-16 22:17:25 -0500

    new view

---
+++ views/default.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+function Def (html) {
+    <html>
+        <head>
+            <title>The Title is here</title>
+        </head>
+        <body>
+            {html.children}
+        </body>
+    </html>
+}
+module.exports = Def
