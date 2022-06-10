c5673e54a56e5f08b474a46513622d32801635ab
AugieDog <67617205+Augiedog@users.noreply.github.com>
2022-06-10 08:56:25 -0500

    Update places.js

--- models/places.js
+++ models/places.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose')
 
+// Addded places at the bottom
 const placeSchema = new mongoose.Schema({
     name: { type: String, require: true},
     pic: { type: String, default: './public/images/IMG_20190817_184559480_HDR.jpg'},
     cuisines: { type: String, require},
     city: { type: String, default: 'Anytown' },
     state: { type: String, default: 'USA'},
-    founded: Number
+    founded: Number,
+    places: {type: mongoose.Schema.Types.ObjectId, ref: 'Places'}
 })
 
 module.exports = mongoose.model('Place', placeSchema)
